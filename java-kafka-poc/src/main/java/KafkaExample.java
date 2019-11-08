ef9488d2c45b95e3afb80f769efc38f8d8d34820
​Srilekha Rajendran <Srilekha@amzurtech.com>
2019-11-08 18:26:07 +0530

    simple kafka poc with maven

---
+++ java-kafka-poc/src/main/java/KafkaExample.java
@@ -0,0 +1,93 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class KafkaExample {
+    private final String topic;
+    private final Properties props;
+
+    public KafkaExample(String brokers, String username, String password) {
+        this.topic = username + "-1";
+
+      //  String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+       // String jaasCfg = String.format(jaasTemplate, username, password);
+
+        String serializer = StringSerializer.class.getName();
+        String deserializer = StringDeserializer.class.getName();
+        props = new Properties();
+        props.put("bootstrap.servers", brokers);
+        props.put("group.id", username + "-consumer");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "earliest");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", deserializer);
+        props.put("value.deserializer", deserializer);
+        props.put("key.serializer", serializer);
+        props.put("value.serializer", serializer);
+     //   props.put("security.protocol", "SASL_SSL");
+      //  props.put("sasl.mechanism", "SCRAM-SHA-256");
+      //  props.put("sasl.jaas.config", jaasCfg);
+    }
+
+    public void consume() {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("%s [%d] offset=%d, key=%s, value=\"%s\"\n",
+								  record.topic(), record.partition(),
+								  record.offset(), record.key(), record.value());
+			}
+        }
+    }
+
+    public void produce() {
+        Thread one = new Thread() {
+            public void run() {
+            	 System.out.println("Sending........1....");
+                try {
+                    Producer<String, String> producer = new KafkaProducer<>(props);
+                    int i = 0;
+                   // while(true) 
+                    for(int j =0;j<10;j++)
+                    {
+                        Date d = new Date();
+                        try {
+							producer.send(new ProducerRecord<>(topic, Integer.toString(i), d.toString())).get();
+						} catch (ExecutionException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+                        System.out.println("Sending.......2.....");
+                        Thread.sleep(10);
+                        i++;
+                    }
+                } catch (InterruptedException v) {
+                    System.out.println(v);
+                }
+            }
+        };
+        one.start();
+    }
+
+    public static void main(String[] args) {
+		String brokers = "localhost:9092";
+		String username = "srilekha";
+		String password = "srilekha";
+		KafkaExample c = new KafkaExample(brokers, username, password);
+        c.produce();
+        c.consume();
+    }
+}
